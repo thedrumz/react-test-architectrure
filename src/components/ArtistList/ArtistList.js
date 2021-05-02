dcd00e282aad994953b814f06d318b010c0edcf2
max <max@glue.digital>
2021-05-02 21:10:49 +0200

    center search no found message

--- src/components/ArtistList/ArtistList.js
+++ src/components/ArtistList/ArtistList.js
@@ -6,27 +6,40 @@ import { FormattedMessage } from "react-intl";
 import "./ArtistList.sass";
 
 const ArtistList = ({ artists }) => {
-  return (
+  return !artists?.length ? (
+    <NoResults />
+  ) : (
     <div className="artist-list">
-      {!artists?.length && (
-        <p role="status">
-          <FormattedMessage id="search.no-results" />
-        </p>
-      )}
-      {artists.map((artist) => (
-        <ArtistCard
-          key={artist.id}
-          name={artist.name}
-          country={artist.country}
-          disambiguation={artist.disambiguation}
-          tags={artist.tags}
-        />
-      ))}
+      <List artists={artists} />
     </div>
   );
 };
 
+const NoResults = () => {
+  return (
+    <p className="artist-list__no-results" role="status">
+      <FormattedMessage id="search.no-results" />
+    </p>
+  );
+};
+
+const List = ({ artists }) => {
+  return artists.map((artist) => (
+    <ArtistCard
+      key={artist.id}
+      name={artist.name}
+      country={artist.country}
+      disambiguation={artist.disambiguation}
+      tags={artist.tags}
+    />
+  ));
+};
+
 ArtistList.propTypes = {
+  artists: PropTypes.array.isRequired,
+};
+
+List.propTypes = {
   artists: PropTypes.array.isRequired,
 };
 
